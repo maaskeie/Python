0a47bf16ca56ebe668ca4a1aa7fe050817c98e92
Magnus Aagaard Skeie <magnus.aagaard.skeie@kystverket.no>
2020-09-04 15:33:05 +0200

    Included work on for loops.

--- EgentreningP4E.py
+++ EgentreningP4E.py
@@ -14,7 +14,7 @@ while balance < TARGET:
     interest = INTERESTRATE * balance
     balance = balance + interest
     print(balance,year)
-    
+     
 count = 0
 while count < 10:
     count += 1
@@ -30,7 +30,6 @@ while inputStr != "" :
     inputStr = input("Oppgi et beløp med 1 desimal :")
 print("Summen er",total)
     
-
 total = 0.0
 count = 0
 inputStr =input("Oppgi et beløp med 1 desimal (bruk punktum som skilletegn) :")
@@ -46,4 +45,97 @@ else :
     average = 0.0
     print("Ingen tall er oppgitt. Gjennomsnittet er",average)
     
+#Comparing adjacent values
+value = 0.0
+inputStr = input("Enter an integer value: ")
+while inputStr != "" :
+    previous = value
+    value = float(inputStr) 
+    if value == previous :
+        print("Duplicate input")
+    inputStr = input("Enter an integer value: ")
     
+#For loops
+for i in range(1,10,2) :
+    print(i)
+
+for number in range(10) :
+    print(number)
+    
+count = 0
+for i in range(1,1000000,2) :
+    count += 1
+print("Det er",count,"oddetall i sekvensen.")
+
+#Printing a table
+
+#Initialize maximum values for the max ranges (determining no. of rows and columns)
+NMAX = 4
+XMAX = 10
+
+#Print tabel header
+print()
+for n in range(1, NMAX + 1) :
+    print("%10d" % n, end="")
+    
+print()
+for n in range(1, NMAX + 1) :
+    print("%10s" % "x", end="")
+
+print("\n", "      ", "-" * 33)
+
+#Print table body
+for x in range(1, XMAX + 1):
+    for n in range(1, NMAX + 1):
+        print("%10.0f" % x ** n, end="" )
+    print()
+
+#Printing some patterns
+
+for i in range(5):
+    for j in range(4):
+        print("*", end="")
+    print()
+    
+for i in range(10):
+    for j in range(50):
+        if i % 2 == 1: 
+            if j % 2 == 1:
+                print("*", end="")
+            else:
+                print("-", end="")
+        else:
+            if j % 2 == 0:
+                print("*", end="")
+            else:
+                print("-", end="")
+    print()
+    
+#Evaluating strings
+#Finding matches    
+
+found = False
+count = 0
+while not found:
+    if count < 10:
+        print("a")
+        count += 1
+    else:
+        found = True
+        print(found)
+  
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
